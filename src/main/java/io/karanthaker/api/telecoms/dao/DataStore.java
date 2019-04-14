7fec29e2df868a6c0448f74a95f73b7842b883f0
p-karanthaker <p-karanthaker@users.noreply.github.com>
2019-04-14 19:18:10 +0100

    Retrieve Numbers For Given Customer
    Tests passing for retrieval of phone numbers for a given customer.

--- src/main/java/io/karanthaker/api/telecoms/dao/DataStore.java
+++ src/main/java/io/karanthaker/api/telecoms/dao/DataStore.java
@@ -1,11 +1,18 @@
 package io.karanthaker.api.telecoms.dao;
 
+import io.karanthaker.api.telecoms.model.Customer;
 import io.karanthaker.api.telecoms.model.PhoneNumber;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /** Singleton class to store the telecoms data. */
 public class DataStore {
+
+  /** Maps Customer Ids to Customers. */
+  private HashMap<Integer, Customer> customers = new HashMap<>();
 
   /** List to store all phone numbers. */
   private List<PhoneNumber> numbers = new ArrayList<>();
@@ -21,11 +28,15 @@ public class DataStore {
     return instance;
   }
 
-  /** Initialise DataStore with 10 PhoneNumbers. */
+  /** Initialise DataStore with 10 PhoneNumbers and 3 customers. */
   private DataStore() {
-    for (int i = 0; i < 10; i++) {
+    for (int i = 1; i < 11; i++) {
       numbers.add(new PhoneNumber(Integer.toString(i)));
     }
+
+    customers.put(1, new Customer(1, new HashSet<>(numbers.subList(0, 3))));
+    customers.put(2, new Customer(2, new HashSet<>(numbers.subList(3, 7))));
+    customers.put(3, new Customer(3, new HashSet<>(numbers.subList(7, 10))));
   }
 
   /**
@@ -34,5 +45,14 @@ public class DataStore {
    */
   public List<PhoneNumber> getAllNumbers() {
     return numbers;
+  }
+
+  public Set<PhoneNumber> getCustomersNumbers(int customerId) {
+    if (customers.containsKey(customerId)) {
+      final Customer customer = customers.get(customerId);
+      return customer.getNumbers();
+    } else {
+      return new HashSet<>();
+    }
   }
 }
