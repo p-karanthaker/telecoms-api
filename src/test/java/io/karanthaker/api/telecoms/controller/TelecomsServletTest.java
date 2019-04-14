c77b140eacc10a3d9d20af21122b721e670f0068
p-karanthaker <p-karanthaker@users.noreply.github.com>
2019-04-14 20:21:50 +0100

    (Failing Test) Test Endpoint For All Numbers
    Added test to check that the servlet returns the correct response when
    requesting the "/numbers" endpoint. Using Mockito to mock out the
    servlet requests and responses.

--- src/test/java/io/karanthaker/api/telecoms/controller/TelecomsServletTest.java
+++ src/test/java/io/karanthaker/api/telecoms/controller/TelecomsServletTest.java
@@ -1,16 +1,41 @@
 package io.karanthaker.api.telecoms.controller;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 @DisplayName("Telecoms API Tests")
 public class TelecomsServletTest {
 
+  private static TelecomsServlet servlet;
+  private static HttpServletRequest request;
+  private static HttpServletResponse response;
+  private static StringWriter stringWriter;
+
+  @BeforeAll
+  public static void setUp() throws Exception {
+    servlet = new TelecomsServlet();
+    request = Mockito.mock(HttpServletRequest.class);
+    response = Mockito.mock(HttpServletResponse.class);
+
+    stringWriter = new StringWriter();
+    PrintWriter writer = new PrintWriter(stringWriter);
+    Mockito.when(response.getWriter()).thenReturn(writer);
+  }
+
   @Test
-  @DisplayName("Dummy Test")
-  public void pass() {
-    Assertions.assertTrue(true);
+  @DisplayName("Endpoint - Get All Phone Numbers")
+  public void testRetrievalOfAllPhoneNumbers() throws Exception {
+    Mockito.when(request.getPathInfo()).thenReturn("/numbers");
+    servlet.doGet(request, response);
+
+    Assertions.assertEquals("Get All Phone Numbers", stringWriter.toString());
   }
 
 }
