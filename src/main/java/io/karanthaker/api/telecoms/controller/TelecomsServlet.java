c274431f8bfdb99609eca4b7dfc8e9e260f1392e
p-karanthaker <p-karanthaker@users.noreply.github.com>
2019-04-14 20:24:27 +0100

    All Numbers Enpoint Implemented
    Implemented minimal code to pass the test which returns all phone
    numbers.

--- src/main/java/io/karanthaker/api/telecoms/controller/TelecomsServlet.java
+++ src/main/java/io/karanthaker/api/telecoms/controller/TelecomsServlet.java
@@ -1,7 +1,22 @@
 package io.karanthaker.api.telecoms.controller;
 
+import java.io.IOException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class TelecomsServlet extends HttpServlet {
 
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    final String action = req.getPathInfo();
+
+    switch (action) {
+      case "/numbers":
+        resp.getWriter().write("Get All Phone Numbers");
+        break;
+      default:
+        break;
+    }
+  }
 }
