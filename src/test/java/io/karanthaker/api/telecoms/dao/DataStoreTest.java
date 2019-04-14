c79222af8342199b7b2713b424b72ffd7a9a2b6d
p-karanthaker <p-karanthaker@users.noreply.github.com>
2019-04-14 19:10:26 +0100

    (Failing Test) Get Numbers For Specific Customer
    Test added to retrieve the phone numbers of a given customer.

--- src/test/java/io/karanthaker/api/telecoms/dao/DataStoreTest.java
+++ src/test/java/io/karanthaker/api/telecoms/dao/DataStoreTest.java
@@ -1,5 +1,8 @@
 package io.karanthaker.api.telecoms.dao;
 
+import io.karanthaker.api.telecoms.model.PhoneNumber;
+import java.util.HashSet;
+import java.util.Set;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,6 +16,17 @@ public class DataStoreTest {
   @DisplayName("Test Retrieving All Numbers")
   public void retrieveAllNumbers() {
     Assertions.assertEquals(10, dataStore.getAllNumbers().size());
+  }
+
+  @Test
+  @DisplayName("Get Numbers For Specific Customer")
+  public void testCustomersNumbers() {
+    Set<PhoneNumber> expected = new HashSet<>();
+    expected.add(new PhoneNumber("1"));
+    expected.add(new PhoneNumber("2"));
+    expected.add(new PhoneNumber("3"));
+
+    Assertions.assertEquals(expected, dataStore.getCustomersNumbers(1));
   }
 
 }
