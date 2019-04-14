2a8d8015316ec602a34e20b3c1dd33e7146d186b
p-karanthaker <p-karanthaker@users.noreply.github.com>
2019-04-14 21:43:59 +0100

    (Failing Test) Get Customer's Phone Numbers
    Added unit test for retrieving a given customer's phone numbers.

--- src/test/java/io/karanthaker/api/telecoms/controller/TelecomsServletTest.java
+++ src/test/java/io/karanthaker/api/telecoms/controller/TelecomsServletTest.java
@@ -2,10 +2,14 @@ package io.karanthaker.api.telecoms.controller;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.HashSet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -13,13 +17,13 @@ import org.mockito.Mockito;
 @DisplayName("Telecoms API Tests")
 public class TelecomsServletTest {
 
-  private static TelecomsServlet servlet;
-  private static HttpServletRequest request;
-  private static HttpServletResponse response;
-  private static StringWriter stringWriter;
+  private TelecomsServlet servlet;
+  private HttpServletRequest request;
+  private HttpServletResponse response;
+  private StringWriter stringWriter;
 
-  @BeforeAll
-  public static void setUp() throws Exception {
+  @BeforeEach
+  private void setUp() throws Exception {
     servlet = new TelecomsServlet();
     request = Mockito.mock(HttpServletRequest.class);
     response = Mockito.mock(HttpServletResponse.class);
@@ -36,6 +40,23 @@ public class TelecomsServletTest {
     servlet.doGet(request, response);
 
     final String expectedJson = "[{ \"number\": 1, \"activated\": false }, { \"number\": 2, \"activated\": false }, { \"number\": 3, \"activated\": false }, { \"number\": 4, \"activated\": false }, { \"number\": 5, \"activated\": false }, { \"number\": 6, \"activated\": false }, { \"number\": 7, \"activated\": false }, { \"number\": 8, \"activated\": false }, { \"number\": 9, \"activated\": false }, { \"number\": 10, \"activated\": false }]";
+    Assertions.assertEquals(expectedJson, stringWriter.toString());
+  }
+
+  @Test
+  @DisplayName("Endpoint - Get Customer's Phone Numbers")
+  public void testRetrievalOfACustomersNumbers() throws Exception {
+    Mockito.when(request.getPathInfo()).thenReturn("/numbers/");
+    Mockito.when(request.getParameter("customer")).thenReturn("1");
+
+    final HashMap<String, String> params = new HashMap<>();
+    params.put("customer", "1");
+    Mockito.when(request.getParameterMap()).thenReturn(params);
+
+    servlet.doGet(request, response);
+    Mockito.verify(request, Mockito.atLeast(1)).getParameter("customer");
+
+    final String expectedJson = "[{ \"number\": 1, \"activated\": false }, { \"number\": 2, \"activated\": false }, { \"number\": 3, \"activated\": false }]";
     Assertions.assertEquals(expectedJson, stringWriter.toString());
   }
 
