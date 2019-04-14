b72dd8d77aa985c36b99c050f19d2b03eecdba7a
p-karanthaker <p-karanthaker@users.noreply.github.com>
2019-04-14 20:35:18 +0100

    Get All Numbers Endpoint
    Tests for "Get All Numbers" (/numbers) endpoint is now working and
    returning a valid JSON array of PhoneNumber objects.

--- src/main/java/io/karanthaker/api/telecoms/controller/TelecomsServlet.java
+++ src/main/java/io/karanthaker/api/telecoms/controller/TelecomsServlet.java
@@ -1,6 +1,10 @@
 package io.karanthaker.api.telecoms.controller;
 
+import io.karanthaker.api.telecoms.dao.DataStore;
+import io.karanthaker.api.telecoms.model.PhoneNumber;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,13 +14,20 @@ public class TelecomsServlet extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
     final String action = req.getPathInfo();
+    final PrintWriter out = resp.getWriter();
+    resp.setContentType("application/json");
+    resp.setCharacterEncoding("UTF-8");
+    final DataStore dataStore = DataStore.getInstance();
 
     switch (action) {
       case "/numbers":
-        resp.getWriter().write("Get All Phone Numbers");
+        final List<PhoneNumber> numbers = dataStore.getAllNumbers();
+        out.print(numbers.toString());
         break;
       default:
         break;
     }
+    
+    out.flush();
   }
 }
